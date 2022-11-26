d12d3025524c543b194ab9b0b9b9377dfc82739a
Shirsho Dipto <shirshodipto@gmail.com>
2022-11-26 11:35:35 +0600

    Adjust background for the homepage

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './homepage.css'
+
+const content = document.querySelector('#content');
+console.log(content);
+// content.classList.add('hello');
+console.log('it is working if this is printed');
